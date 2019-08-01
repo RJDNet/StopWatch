8a36205c169074403d8110e7fa21c055d6024846
Dando <P10498600@capita.co.uk>
2019-08-01 10:08:47 +0100

    CSS Updates

--- src/StopWatchContainer/Components/StopWatchContainer.tsx
+++ src/StopWatchContainer/Components/StopWatchContainer.tsx
@@ -20,10 +20,13 @@ export const StopWatchContainer: React.FC<IComponentProps> = (props): JSX.Elemen
 		dispatch,
 		stopWatchList
 	} = props;
+
 	const [amount, setAmount] = React.useState('');
 
 	const DEFAULT_CLASSNAME: string = 'stopwatches-container';
 	const HEADER_CLASSNAME: string = `${DEFAULT_CLASSNAME}__header`;
+	const COUNTER_HEADER_CLASSNAME: string = `${DEFAULT_CLASSNAME}__counter-header`;
+	const COUNTER_CLASSNAME: string = `${DEFAULT_CLASSNAME}__counter`;
 	const BUTTON_CONTAINER_CLASSNAME: string = `${DEFAULT_CLASSNAME}__button-container`;
 	const BUTTON_CONTAINER_ADD_BUTTON_CLASSNAME: string = `${BUTTON_CONTAINER_CLASSNAME}__add-button`;
 	const BUTTON_CONTAINER_ADDX_BUTTON_CLASSNAME: string = `${BUTTON_CONTAINER_CLASSNAME}__addx-button`;
@@ -58,6 +61,8 @@ export const StopWatchContainer: React.FC<IComponentProps> = (props): JSX.Elemen
 	return (
 		<div className={DEFAULT_CLASSNAME}>
 			<h1 className={HEADER_CLASSNAME}>Stopwatch App</h1>
+			<p className={COUNTER_HEADER_CLASSNAME}>Stopwatch Count</p>
+			<p className={COUNTER_CLASSNAME}>{stopWatchList.length}</p>
 			<div className={BUTTON_CONTAINER_CLASSNAME}>
 				<button className={BUTTON_CONTAINER_ADD_BUTTON_CLASSNAME} onClick={addStopWatch}>ADD ONE</button>
 				<button className={BUTTON_CONTAINER_REMOVE_BUTTON_CLASSNAME} onClick={removeAllStopWatches}>REMOVE ALL</button>
